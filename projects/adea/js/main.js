eadd8bdd74ba7485bb63612beb66b37207db2438
nelson <nelsonlemmy61@gmail.com>
2023-04-07 21:51:48 +0300

    delete project adea

--- projects/adea/js/main.js
+++
@@ -1,7 +0,0 @@
-const pages = document.querySelectorAll(".page");
-const imageContainer = document.querySelector('.main-content-image-container');
-const cahngeViewInner = document.querySelector('.change-view-inner');
-const imageView = document.querySelector('.image-view');
-const videoView = document.querySelector('.video-view');
-const translateAmount = 100; 
-
